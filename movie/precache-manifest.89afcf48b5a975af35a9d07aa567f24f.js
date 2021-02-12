790a2ddd397661a8fbcfb933af0be03111ffd8ee
madchick <madchick@gmail.com>
2021-02-13 01:39:27 +0900

    Add some codes

---
+++ movie/precache-manifest.89afcf48b5a975af35a9d07aa567f24f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf66150de836225df9ec8ec74add24fb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4d84c65d245b5ed235cd",
+    "url": "./static/css/main.672a5149.chunk.css"
+  },
+  {
+    "revision": "3292616cc5c511783277",
+    "url": "./static/js/2.558ee272.chunk.js"
+  },
+  {
+    "revision": "4d84c65d245b5ed235cd",
+    "url": "./static/js/main.d945746b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
---
+++ nomflix/precache-manifest.a28cea8b9e07d538de4e76f7104d0af3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f3b17692e82938982a565aab1fdbea",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6136baa863bd3412e8fc",
+    "url": "./static/js/2.bec5ee7b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.bec5ee7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65bb8853db1b74d81574",
+    "url": "./static/js/main.801fb7bd.chunk.js"
+  },
+  {
+    "revision": "fb54ccddb9365ba2651c",
+    "url": "./static/js/runtime-main.fe7eadf5.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "./static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
